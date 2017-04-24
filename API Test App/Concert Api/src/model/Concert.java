346b99776366f65817a43cfc1cf436db06a37d62
Furkan Şenharputlu <furkan@Furkan-MacBook-Air.local>
2017-04-24 19:21:22 +0300

    concert create and getAllConcerts

---
+++ API Test App/Concert Api/src/model/Concert.java
@@ -0,0 +1,32 @@
+package model;
+
+public class Concert {
+	private int id;
+	private String artist;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getArtist() {
+		return artist;
+	}
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	private String location;
+	private String date;
+}
